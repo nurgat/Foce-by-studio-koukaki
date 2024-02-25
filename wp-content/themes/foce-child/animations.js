f0f3cc278a1fa939cb186b83d02237775013f2e8
nurgat <142540895+nurgat@users.noreply.github.com>
2024-02-25 23:04:01 +0400

    Apparaition fade in et Parallax banniere/logo

--- wp-content/themes/foce-child/animations.js
+++ wp-content/themes/foce-child/animations.js
@@ -2,41 +2,68 @@
 
 // Fonction pour ajouter la classe 'visible' aux éléments quand ils entrent dans le viewport
 
-jQuery(function($) {
-    // Fonction pour vérifier et révéler les éléments lors du défilement
-    const revealOnScroll = function() {
-      // Sélectionne tous les éléments ayant la classe 'fade-in'
-      const elements = $('.fade-in');
+// animations.js
+
+// Fonction pour ajouter la classe 'visible' aux éléments quand ils entrent dans le viewport
+//document.addEventListener("DOMContentLoaded", function() {
+    // Créer un observer pour détecter l'entrée des sections dans le viewport
+  //  let observer = new IntersectionObserver((entries, observer) => {
+  //    entries.forEach(entry => {
+   //     if (entry.isIntersecting) {
+   //       entry.target.classList.add('visible');
+    //      observer.unobserve(entry.target); // Optionnel: arrêter d'observer une fois visible
+     //   }
+     // });
+    //}, { threshold: 0.1 }); // Seuil pour déclencher l'observation
   
-      // Utilise une boucle for pour itérer sur tous les éléments sélectionnés
-      for (let i = 0; i < elements.length; i++) {
-        // Accède à chaque élément individuellement
-        const element = $(elements[i]);
+    // Sélectionner toutes les sections à animer
+   // const sections = document.querySelectorAll('.fade-in');
   
-        // Obtient la position de l'élément actuel par rapport au haut du document
-        const elementTop = element.offset().top;
-  
-        // Obtient la hauteur de la fenêtre du navigateur
-        const windowHeight = $(window).height();
-  
-        // Vérifie si l'élément est visible dans le viewport, à 75% de la hauteur de la fenêtre
-        const isVisible = elementTop < $(window).scrollTop() + windowHeight * 0.75;
-  
-        // Si l'élément est visible, ajoute la classe 'visible'
-        if (isVisible) {
-          element.addClass('visible');
+    // Observer chaque section
+    //sections.forEach(section => {
+      //observer.observe(section);
+   // });
+  //});
+
+  jQuery(document).ready(function($) {
+    // Déclare une fonction pour vérifier si les sections doivent être affichées avec un effet fade-in
+    function checkFadeInSection() {
+        var fadeIns = document.querySelectorAll('.fade-in-section');
+        
+        for (var i = 0; i < fadeIns.length; i++) {
+            var elemPos = $(fadeIns[i]).offset().top;
+            var scrollTop = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            
+            // Déclenche l'animation si l'élément est déjà visible au chargement de la page, ou lors du scroll
+            if (scrollTop + windowHeight - 100 > elemPos || windowHeight + 100 > elemPos) {
+                $(fadeIns[i]).addClass('fade-in-up');
+            }
         }
-      }
-    };
+    }
+
+    // Exécute immédiatement pour vérifier si des éléments doivent être animés au chargement de la page
+    checkFadeInSection();
+
+    // Continue de vérifier lors du défilement
+    $(window).scroll(function() {
+        checkFadeInSection();
+    });
+});
+
+const parallax = document.querySelector('.parallax');
+
+window.addEventListener('scroll', () => {
+    //console.log(window.scrollY / 2)
+    parallax.style.backgroundPositionY = -window.scrollY / 2.45 + "px";
+});
+
+
+
+
+
   
-    // Écoute l'événement de défilement sur la fenêtre et appelle revealOnScroll à chaque défilement
-    // Utilise .on() au lieu de .scroll() pour attacher le gestionnaire d'événement
-    $(window).on('scroll', revealOnScroll);
   
-    // Appelle également revealOnScroll au chargement initial au cas où des éléments sont déjà visibles
-    revealOnScroll();
-  });
-  
-  
+
   
   
--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -1,7 +1,8 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
 
 // Intégration des dépendances
@@ -16,27 +17,35 @@ function theme_enqueue_scripts_and_styles() {
 
     // GSAP
     wp_enqueue_script('gsap-js', get_stylesheet_directory_uri() . '/node_modules/gsap/dist/gsap.min.js', array(), false, true);
+
+    // SIMPLE-PARALLAX
+    //wp_enqueue_script('simple-parallax-js', get_stylesheet_directory_uri() . '/node_modules/simple-parallax-js/dist/simpleParallax.min.js', array(), false, true);
+    
 }
 
 add_action('wp_enqueue_scripts', 'theme_enqueue_scripts_and_styles');
 
-// Get customizer options form parent theme
-if ( get_stylesheet() !== get_template() ) {
-    add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
-        update_option( 'theme_mods_' . get_template(), $value );
-        return $old_value; // prevent update to child theme mods
-    }, 10, 2 );
-    add_filter( 'pre_option_theme_mods_' . get_stylesheet(), function ( $default ) {
-        return get_option( 'theme_mods_' . get_template(), $default );
-    } );
+// Gère l'héritage des options du customiseur du thème parent
+if (get_stylesheet() !== get_template()) {
+    add_filter('pre_update_option_theme_mods_' . get_stylesheet(), function ($value, $old_value) {
+        update_option('theme_mods_' . get_template(), $value);
+        return $old_value; // Empêche la mise à jour des mods du thème enfant
+    }, 10, 2);
+
+    add_filter('pre_option_theme_mods_' . get_stylesheet(), function ($default) {
+        return get_option('theme_mods_' . get_template(), $default);
+    });
 }
 
 function foce_child_enqueue_scripts() {
-    // Assurez-vous que jQuery est chargé sur le site
+    // Assurez-vous que jQuery est chargé sur le site si nécessaire
     wp_enqueue_script('jquery');
     
-    // Ajoute votre script personnalisé avec jQuery comme dépendance
-    wp_enqueue_script('foce-child-animations', get_stylesheet_directory_uri() . '/animations.js', array('jquery'), '1.0.0', true);
+    // Ajoute votre script personnalisé sans jQuery comme dépendance
+    wp_enqueue_script('foce-child-custom-js', get_stylesheet_directory_uri() . '/animations.js', array(), '1.0.0', true);
+
+    // Enregistre et met en file d'attente le fichier CSS compilé du thème enfant
+    wp_enqueue_style('foce-child-style', get_stylesheet_directory_uri() . '/css/index.css', array(), '1.0.0');
 }
 
 // Ajoute l'action pour exécuter la fonction définie ci-dessus
